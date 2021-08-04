472461f0dce47f10d2ac2bbfc534f67024b0a7c0
Chris Campbell <chris.campbell1@live.com>
2021-08-04 15:27:22 -0700

    Create buttons func

--- budget_gui.py
+++ budget_gui.py
@@ -1,9 +1,4 @@
 from tkinter import *
-from dataclasses import dataclass
-
-@dataclass
-class AppConfig:
-    pass
 
 class BudgetGui(Frame):
     def __init__(self, master=None):
@@ -11,9 +6,16 @@ class BudgetGui(Frame):
         self.master = master
         self.pack()
         self.create_menu()
-
+        self.button_dict = {
+            'create': self.create_expense,
+            'show': self.show_expenses,
+            'edit': self.edit_expense,
+            'delete': self.delete_expense
+        }
+        self.create_buttons()
+        
     def create_menu(self):
-        Label(self, text='Main Menu').pack()
+        Label(self, text='Main Menu').pack(side=TOP)
 
     def show_expenses(self):
         pass
@@ -26,6 +28,10 @@ class BudgetGui(Frame):
 
     def delete_expense(self):
         pass
+
+    def create_buttons(self):
+        for button, func in self.button_dict.items():
+            Button(self, text=button.capitalize(), command=func).pack(side=TOP)
 
 
 
