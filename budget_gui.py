d4d28416215c514f1decaea39f69b94eb5d22c66
Chris Campbell <chris.campbell1@live.com>
2021-08-05 13:07:45 -0700

    Create_button & toplevel funcs

--- budget_gui.py
+++ budget_gui.py
@@ -3,23 +3,33 @@ from tkinter import *
 class BudgetGui(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.master = master
-        self.pack()
-        self.create_menu()
         self.button_dict = {
             'create': self.create_expense,
             'show': self.show_expenses,
             'edit': self.edit_expense,
             'delete': self.delete_expense
         }
+        self.master = master
+        self.pack()
+        self.create_menu()
         self.create_buttons()
-        
+
     def create_menu(self):
         Label(self, text='Main Menu').pack(side=TOP)
 
+# create generic toplevel window, use button dict to insert appropriate logic?
+    def create_buttons(self):
+        for button, func in self.button_dict.items():
+            Button(self, text=button.capitalize(), command=lambda button=button, func=func: self.create_toplevel(button, func)).pack(side=TOP)
+
+    def create_toplevel(self, button, func):
+        frame = Toplevel(self)
+        Label(frame, text=button.capitalize()).pack(side=TOP)
+        func()
+
     def show_expenses(self):
         pass
-
+        
     def create_expense(self):
         pass
 
@@ -29,9 +39,6 @@ class BudgetGui(Frame):
     def delete_expense(self):
         pass
 
-    def create_buttons(self):
-        for button, func in self.button_dict.items():
-            Button(self, text=button.capitalize(), command=func).pack(side=TOP)
 
 
 
