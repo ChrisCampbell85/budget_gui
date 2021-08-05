767e2eebe5b9d9fee8984f37d13a87477144fef8
Chris Campbell <chris.campbell1@live.com>
2021-08-05 15:00:34 -0700

    Create all funcs for create expense

--- budget_gui.py
+++ budget_gui.py
@@ -4,11 +4,18 @@ class BudgetGui(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.button_dict = {
-            'create': self.create_expense,
-            'show': self.show_expenses,
-            'edit': self.edit_expense,
-            'delete': self.delete_expense
+            'Create Expense': self.create_expense,
+            'Show Expenses': self.show_expenses,
+            'Edit Expenses': self.edit_expense,
+            'Delete Expenses': self.delete_expense
         }
+        self.expense_frequency = {
+            'Daily': 365,
+            'Weekly': 52,
+            'Monthly': 12,
+            'Yearly': 1
+        }
+        self.yearly_expense_dict = {}
         self.master = master
         self.pack()
         self.create_menu()
@@ -17,29 +24,56 @@ class BudgetGui(Frame):
     def create_menu(self):
         Label(self, text='Main Menu').pack(side=TOP)
 
-# create generic toplevel window, use button dict to insert appropriate logic?
+# create generic toplevel window, use button_name dict to insert appropriate logic?
     def create_buttons(self):
-        for button, func in self.button_dict.items():
-            Button(self, text=button.capitalize(), command=lambda button=button, func=func: self.create_toplevel(button, func)).pack(side=TOP)
+        for button_name, func in self.button_dict.items():
+            Button(self, text=button_name, command=lambda button_name=button_name, func=func: self.create_toplevel(button_name, func)).pack(side=TOP)
 
-    def create_toplevel(self, button, func):
+    def create_toplevel(self, button_name, func):
         frame = Toplevel(self)
-        Label(frame, text=button.capitalize()).pack(side=TOP)
-        func()
+        Label(frame, text=button_name).pack(side=TOP)
+        func(frame)
+
+    def create_expense(self, frame):
+        Label(frame, text='Please enter an expense to create').pack(side=TOP, fill=X)
+        create_text = StringVar()
+        Entry(frame, textvariable=create_text).pack(side=TOP)
+        Button(frame, text='Add Amount', command=lambda: self.add_amount(frame, create_text)).pack(anchor=SE)
+        
+    def add_amount(self, frame, create_text):
+        Label(frame, text='Please enter amount').pack(side=TOP)
+        print(create_text.get())
+        add_amount_text = StringVar()
+        Entry(frame, textvariable=add_amount_text).pack(side=TOP)
+        Button(frame, text='Expense Frequency', command=lambda: self.expense_frequency_calc(frame, create_text, add_amount_text)).pack(anchor=SE)
+
+    def expense_frequency_calc(self, frame, create_text, add_amount_text):
+        Label(frame, text='Please select expense frequency').pack(side=TOP)
+        print(add_amount_text.get())
+        freq_value = IntVar()
+        for freq, value in self.expense_frequency.items():
+            Radiobutton(frame, text=freq, variable=freq_value, value=value).pack(side=TOP)
+        Button(frame, text='Save', command=lambda: self.calculate_yearly_expense(frame, create_text, add_amount_text, freq_value)).pack(anchor=SE)
+        
+
+    def calculate_yearly_expense(self, frame, create_text, add_amount_text, freq_value):
+        frame.destroy()
+        expense_amount = int(add_amount_text.get()) * int(freq_value.get())
+        self.yearly_expense_dict[create_text.get()] = expense_amount
+        print(self.yearly_expense_dict)
+        
 
     def show_expenses(self):
         pass
         
-    def create_expense(self):
-        pass
-
     def edit_expense(self):
         pass
 
     def delete_expense(self):
         pass
 
-
+    def quit_button(self):
+        pass
 
 
 if __name__ == '__main__':
